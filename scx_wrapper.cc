bf0b1cc0958e6da5b6c73cc417ab86d91b951e0c
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2025-01-06 21:08:16 +0900

    Create SCX-specific tracks
    Before this commit, all trace events were placed on
    a single `ThreadTrack`, making it difficult to interpret
    the data. This change introduces per-CPU tracks specific
    to SCX, improving readability and organization of trace
    events.

--- scx_wrapper.cc
+++ scx_wrapper.cc
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <cstdio>
 #include <perfetto.h>
 #include <fcntl.h>
 
@@ -13,6 +14,31 @@ PERFETTO_TRACK_EVENT_STATIC_STORAGE();
 
 static std::unique_ptr<perfetto::TracingSession> tracing_session;
 static int fd;
+
+/*
+ * UUID of the track that contains scx events.
+ * If you want to know about "Track" more, see the comment on perfetto::Track.
+ */
+#define SCX_TRACK_UUID	0xdeadbeefcafebabe
+#define SCX_TRACK_UUID_CPU(cpu) (SCX_TRACK_UUID + cpu)
+
+perfetto::Track get_track(s32 cpu)
+{
+	return perfetto::Track::Global(SCX_TRACK_UUID_CPU(cpu));
+}
+
+void set_all_track_names()
+{
+	char track_name[0x20];
+	for (int cpu = 0; cpu < 12 /* NR_CPUS*/; cpu++) {
+		auto track = get_track(cpu);
+		auto desc = track.Serialize();
+
+		snprintf(track_name, 0x20, "SCX (Cpu %d)", cpu);
+		desc.set_name(track_name);
+		perfetto::TrackEvent::SetTrackDescriptor(track, desc);
+	}
+}
 
 void init_perfetto(void)
 {
@@ -95,6 +121,8 @@ void start_perfetto_trace(const char *output_file)
 		return;
 	}
 
+	set_all_track_names();
+
 	tracing_session = perfetto::Tracing::NewTrace();
 	tracing_session->Setup(cfg, fd);
 	tracing_session->StartBlocking();
@@ -111,88 +139,115 @@ void stop_perfetto_trace(void)
 
 void trace_select_cpu(struct entry_header *hdr, struct select_cpu_aux *aux)
 {
+	auto track = get_track(hdr->cpu);
+
 	TRACE_EVENT("scx", "select_cpu",
+		    track,
 		    (uint64_t) hdr->start,
 		    "CPU", hdr->cpu,
 		    "pid", aux->pid,
 		    "prev_cpu", aux->prev_cpu,
 		    "wake_flags", aux->wake_flags,
 		    "selected_cpu", aux->selected_cpu);
-	TRACE_EVENT_END("scx", (uint64_t) hdr->end);
+	TRACE_EVENT_END("scx", track, (uint64_t) hdr->end);
 }
 
 void trace_enqueue(struct entry_header *hdr, struct enqueue_aux *aux)
 {
+	auto track = get_track(hdr->cpu);
+
 	TRACE_EVENT("scx", "enqueue",
+		    track,
 		    (uint64_t) hdr->start,
 		    "CPU", hdr->cpu,
 		    "pid", aux->pid,
 		    "enq_flags", aux->enq_flags);
-	TRACE_EVENT_END("scx", (uint64_t) hdr->end);
+	TRACE_EVENT_END("scx", track, (uint64_t) hdr->end);
 }
 
 void trace_runnable(struct entry_header *hdr, struct runnable_aux *aux)
 {
+	auto track = get_track(hdr->cpu);
+
 	TRACE_EVENT("scx", "runnable",
+		    track,
 		    (uint64_t) hdr->start,
 		    "CPU", hdr->cpu,
 		    "pid", aux->pid,
 		    "enq_flags", aux->enq_flags);
-	TRACE_EVENT_END("scx", (uint64_t) hdr->end);
+	TRACE_EVENT_END("scx", track, (uint64_t) hdr->end);
 }
 
 void trace_running(struct entry_header *hdr, struct running_aux *aux)
 {
+	auto track = get_track(hdr->cpu);
+
 	TRACE_EVENT("scx", "running",
+		    track,
 		    (uint64_t) hdr->start,
 		    "CPU", hdr->cpu,
 		    "pid", aux->pid);
-	TRACE_EVENT_END("scx", (uint64_t) hdr->end);
+	TRACE_EVENT_END("scx", track, (uint64_t) hdr->end);
 }
 
 void trace_stopping(struct entry_header *hdr, struct stopping_aux *aux)
 {
+	auto track = get_track(hdr->cpu);
+
 	TRACE_EVENT("scx", "stopping",
+		    track,
 		    (uint64_t) hdr->start,
 		    "CPU", hdr->cpu,
 		    "pid", aux->pid,
 		    "runnable", aux->runnable ? "true" : "false");
-	TRACE_EVENT_END("scx", (uint64_t) hdr->end);
+	TRACE_EVENT_END("scx", track, (uint64_t) hdr->end);
 }
 
 void trace_quiescent(struct entry_header *hdr, struct quiescent_aux *aux)
 {
+	auto track = get_track(hdr->cpu);
+
 	TRACE_EVENT("scx", "quiescent",
+		    track,
 		    (uint64_t) hdr->start,
 		    "CPU", hdr->cpu,
 		    "pid", aux->pid,
 		    "deq_flags", aux->deq_flags);
-	TRACE_EVENT_END("scx", (uint64_t) hdr->end);
+	TRACE_EVENT_END("scx", track, (uint64_t) hdr->end);
 }
 
 void trace_init_task(struct entry_header *hdr, struct init_task_aux *aux)
 {
+	auto track = get_track(hdr->cpu);
+
 	TRACE_EVENT("scx", "init_task",
+		    track,
 		    (uint64_t) hdr->start,
 		    "CPU", hdr->cpu,
 		    "pid", aux->pid,
 		    "fork", aux->fork);
-	TRACE_EVENT_END("scx", (uint64_t) hdr->end);
+	TRACE_EVENT_END("scx", track, (uint64_t) hdr->end);
 }
 
 void trace_exit_task(struct entry_header *hdr, struct exit_task_aux *aux)
 {
+	auto track = get_track(hdr->cpu);
+
 	TRACE_EVENT("scx", "exit_task",
+		    track,
 		    (uint64_t) hdr->start,
 		    "CPU", hdr->cpu,
 		    "pid", aux->pid,
 		    "cancelled", aux->cancelled);
-	TRACE_EVENT_END("scx", (uint64_t) hdr->end);
+	TRACE_EVENT_END("scx", track, (uint64_t) hdr->end);
 }
 
 void trace_enable(struct entry_header *hdr, struct enable_aux *aux)
 {
+	auto track = get_track(hdr->cpu);
+
 	TRACE_EVENT("scx", "enable",
+		    track,
 		    (uint64_t) hdr->start,
 		    "CPU", hdr->cpu,
 		    "pid", aux->pid);
@@ -201,7 +256,10 @@ void trace_enable(struct entry_header *hdr, struct enable_aux *aux)
 
 void trace_disable(struct entry_header *hdr, struct disable_aux *aux)
 {
+	auto track = get_track(hdr->cpu);
+
 	TRACE_EVENT("scx", "disable",
+		    track,
 		    (uint64_t) hdr->start,
 		    "CPU", hdr->cpu,
 		    "pid", aux->pid);
@@ -210,10 +268,12 @@ void trace_disable(struct entry_header *hdr, struct disable_aux *aux)
 
 void trace_normal(struct entry_header *hdr, void *_aux)
 {
+	auto track = get_track(hdr->cpu);
 	const char *cbstr = get_string_from_cbid(hdr->cbid);
 
 	TRACE_EVENT("scx", perfetto::DynamicString{cbstr},
+		    track,
 		    (uint64_t) hdr->start,
 		    "CPU", hdr->cpu);
-	TRACE_EVENT_END("scx", (uint64_t) hdr->end);
+	TRACE_EVENT_END("scx", track, (uint64_t) hdr->end);
 }
