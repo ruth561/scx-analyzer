6b78bee9fe00f9543466d6d6a32f08134641e56d
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2025-03-27 11:06:45 +0900

    Revise `user_ringbuf_callback` to support DAG task messages
    This commit marks the starting point for implementing a DAG task scheduler.
    It introduces a user ring buffer consumer to process DAG task messages.
    The messsage structures are defined in `src/bpf/dag_bpf_kfuncs.bpf.h`,
    along with kfuncs that implement DAG scheduling algorithms.
    These APIs are not protected by mutual exclusion; only CPU0 currently
    consumes the user ring buffer.

---
+++ scheduler/src/bpf/dag_bpf_kfuncs.bpf.h
@@ -0,0 +1,85 @@
+#ifndef __MY_OPS_KFUNCS_H
+#define __MY_OPS_KFUNCS_H
+
+
+#include "vmlinux.h"
+
+#ifndef __ksym
+#define __ksym __attribute__((section(".ksyms")))
+#endif
+
+#ifndef __weak
+#define __weak __attribute__((weak))
+#endif
+
+#define DAG_TASK_MAX_NODES	20
+#define DAG_TASK_MAX_DEG	20
+#define DAG_TASK_MAX_EDGES	1000
+
+struct node_info {
+	u32 tid;
+	u32 weight;
+
+	s32 prio;
+
+	u32 nr_ins;
+	u32 ins[DAG_TASK_MAX_DEG];
+	u32 nr_outs;
+	u32 outs[DAG_TASK_MAX_DEG];
+};
+
+struct edge_info {
+	u32 from;
+	u32 to;
+};
+
+struct bpf_dag_task {
+	u32 id;
+	u32 nr_nodes;
+	struct node_info nodes[DAG_TASK_MAX_NODES];
+	u32 nr_edges;
+	struct edge_info edges[DAG_TASK_MAX_EDGES];
+};
+
+extern struct bpf_dag_task *bpf_dag_task_alloc(u32 src_node_tid, u32 src_node_weight) __weak __ksym;
+extern void bpf_dag_task_dump(struct bpf_dag_task *dag_task) __weak __ksym;
+extern void bpf_dag_task_free(struct bpf_dag_task *dag_task) __weak __ksym;
+extern s32 bpf_dag_task_add_node(struct bpf_dag_task *dag_task, u32 tid, u32 weight) __weak __ksym;
+extern s32 bpf_dag_task_add_edge(struct bpf_dag_task *dag_task, u32 from, u32 to) __weak __ksym;
+extern void bpf_dag_task_culc_HELT_prio(struct bpf_dag_task *dag_task) __weak __ksym;
+
+enum bpf_dag_msg_type {
+	BPF_DAG_MSG_NEW_TASK,	// 新しいDAGタスクが作成されたことを伝えるメッセージ（DAGタスクの識別番号はsrc nodeのtid）
+	BPF_DAG_MSG_ADD_NODE,
+	BPF_DAG_MSG_ADD_EDGE,
+	BPF_DAG_MSG_COMMIT,	// TODO: このメッセージでやりたいこと
+				//	1. DAGタスクがwell-formedか？
+				//		- 連結か？
+				//		- トポロジカルソートされているか？
+				//		- 変な遷移辺がないか？
+				//		- etc..
+				//	2. 以降のDAGタスクの形状の変更を禁止する
+};
+
+struct bpf_dag_msg_new_task_payload {
+	u32 src_node_tid;
+	u32 src_node_weight;
+};
+
+struct bpf_dag_msg_add_node_payload {
+	u32 dag_task_id;
+	u32 tid;
+	u32 weight;
+};
+
+struct bpf_dag_msg_add_edge_payload {
+	u32 dag_task_id;
+	u32 from_tid;
+	u32 to_tid;
+};
+
+struct bpf_dag_msg_commit_payload {
+	u32 dag_task_id;
+};
+
+#endif /* __MY_OPS_KFUNCS_H */
--- scheduler/src/bpf/sched.bpf.c
+++ scheduler/src/bpf/sched.bpf.c
@@ -5,6 +5,7 @@
 
 #include "intf.h"
 #include "vmlinux.h"
+#include "dag_bpf_kfuncs.bpf.h"
 #include "sched.bpf.h"
 
 #include <bpf/bpf_helpers.h>
@@ -341,16 +342,25 @@ out:
 	return ret;
 }
 
+// MARK: dag_tasks
+#define BPF_DAG_TASK_LIMIT 10
+
+struct dag_tasks_map_value {
+	struct bpf_dag_task __kptr *dag_task;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+	__uint(max_entries, BPF_DAG_TASK_LIMIT);
+	__type(key, s32);
+	__type(value, struct dag_tasks_map_value);
+} dag_tasks SEC(".maps");
+
 // MARK: urb
 /*******************************************************************************
  * Implementation for the user ring buffer
  */
-
-struct msg_struct {
-	s32 pid; /* tid */
-	u64 relative_deadline;
-	u64 exectime;
-};
 
 /*
  * Message queue from userspace to bpf side
@@ -361,53 +371,186 @@ struct {
 	__uint(max_entries, USER_RINGBUF_SIZE);
 } urb SEC(".maps");
 
-/*
- * Callback that is called from consume_user_ringbuf
- */
-static long user_ringbuf_callback(struct bpf_dynptr *dynptr, void *ctx)
+static long handle_new_dag_task(struct bpf_dag_msg_new_task_payload *payload)
 {
-	long err;
-	struct msg_struct msg;
-	struct task_struct *p;
-	struct task_ctx *taskc;
+	s32 key;
+	struct bpf_dag_task *dag_task, *old;
+	long status;
+	struct dag_tasks_map_value local, *v;
 
-	/*
-	 * Copy a messege from dynptr to buf.
-	 */
-	err = bpf_dynptr_read(&msg, sizeof(msg), dynptr, 0, 0);
-	if (err) {
-		bpf_printk("Failed to read from dynptr...");
+	dag_task = bpf_dag_task_alloc(payload->src_node_tid, payload->src_node_weight);
+	if (!dag_task) {
+		bpf_printk("Failed to newly allocate a DAG task (src_node_tid=%d).", payload->src_node_tid);
 		return 1;
 	}
-	bpf_printk("Msg from URB: pid=%d, exectime=%ld, relative_deadline=%ld",
-		msg.pid, msg.exectime, msg.relative_deadline);
-	
-	p = bpf_task_from_pid(msg.pid);
-	if (!p) {
-		bpf_printk("[W] user_ringbuf_callback: Failed to get task_struct from pid");
-		return 0;
-	}	
 
-	taskc = bpf_task_storage_get(&task_ctx, p, NULL, 0);
-	if (!taskc) {
-		bpf_printk("[W] user_ringbuf_callback: Failed to get task storage");
-		bpf_task_release(p);
-		return 0;
+	bpf_printk("Successfully allocates a DAG-task! tid=%d, id=%d", payload->src_node_tid, dag_task->id);
+
+	key = payload->src_node_tid;
+	local.dag_task = NULL;
+	status = bpf_map_update_elem(&dag_tasks, &key, &local, 0);
+	if (status) {
+		bpf_printk("Failed to update dag_tasks's elem with NULL value");
+		bpf_dag_task_free(dag_task);
+		return 1;
 	}
 
-	// bpf_printk("[*] %s[%d] (before): exectime=%ld, relative_deadline=%ld, deadline=%ld",
-        //         p->comm, p->pid, taskc->edf.exectime, taskc->edf.relative_deadline, taskc->edf.deadline);
+	v = bpf_map_lookup_elem(&dag_tasks, &key);
+	if (!v) {
+		bpf_printk("Failed to lookup dag_tasks's elem");
+		bpf_dag_task_free(dag_task);
+		return 1;
+	}
 
-	taskc->edf.sched_hint = msg.exectime;
-	taskc->edf.relative_deadline = msg.relative_deadline;
-	/*
-	 * taskc->edf.deadline deadline is set when task wakes up.
-	 */
+	old = bpf_kptr_xchg(&v->dag_task, dag_task);
 
-        // bpf_printk("[*] %s[%d] (after): exectime=%ld, relative_deadline=%ld, deadline=%ld",
-        //         p->comm, p->pid, taskc->edf.exectime, taskc->edf.relative_deadline, taskc->edf.deadline);
+	if (old)
+		bpf_dag_task_free(old);
 
-	bpf_task_release(p);
+	return 0;
+}
+
+static inline long handle_add_node(struct bpf_dag_msg_add_node_payload *payload)
+{
+	s32 key, node_id;
+	struct bpf_dag_task *dag_task, *old;
+	struct dag_tasks_map_value *v;
+
+	key = payload->dag_task_id;
+	v = bpf_map_lookup_elem(&dag_tasks, &key);
+	if (!v) {
+		bpf_printk("There is no entry in dag_tasks with key=%d", key);
+		return -1;
+	}
+
+	dag_task = bpf_kptr_xchg(&v->dag_task, NULL); // acquire ownership
+	if (!dag_task) {
+		bpf_printk("dag_tasks[%d]->dag_task is NULL", key);
+		return -1;
+	}
+
+	node_id = bpf_dag_task_add_node(dag_task, payload->tid, payload->weight);
+
+	bpf_dag_task_dump(dag_task);
+
+	if (node_id >= 0) {
+		bpf_printk("Successfully add a node (tid=%d, node_id=%d) to a DAG-task (id=%d)",
+			payload->tid, node_id, dag_task->id);
+	} else {
+		bpf_printk("Failed to add a node (tid=%d) to a DAG-task (id=%d)",
+			payload->tid, dag_task->id);
+	}
+
+	old = bpf_kptr_xchg(&v->dag_task, dag_task);
+
+	if (old)
+		bpf_dag_task_free(old);
+
+	return 0;
+}
+
+static inline long handle_add_edge(struct bpf_dag_msg_add_edge_payload *payload)
+{
+	s32 key, edge_id;
+	struct bpf_dag_task *dag_task, *old;
+	struct dag_tasks_map_value *v;
+
+	key = payload->dag_task_id;
+	v = bpf_map_lookup_elem(&dag_tasks, &key);
+	if (!v) {
+		bpf_printk("There is no entry in dag_tasks with key=%d", key);
+		return -1;
+	}
+
+	dag_task = bpf_kptr_xchg(&v->dag_task, NULL); // acquire ownership
+	if (!dag_task) {
+		bpf_printk("dag_tasks[%d]->dag_task is NULL", key);
+		return -1;
+	}
+
+	edge_id = bpf_dag_task_add_edge(dag_task, payload->from_tid, payload->to_tid);
+
+	bpf_dag_task_dump(dag_task);
+
+	if (edge_id >= 0) {
+		bpf_printk("Successfully add a edge (%d -> %d, edge_id=%d) to a DAG-task (id=%d)",
+			payload->from_tid, payload->to_tid, edge_id, dag_task->id);
+	} else {
+		bpf_printk("Failed to add a edge (%d -> %d) to a DAG-task (id=%d)",
+			payload->from_tid, payload->to_tid, dag_task->id);
+	}
+
+	old = bpf_kptr_xchg(&v->dag_task, dag_task);
+
+	if (old)
+		bpf_dag_task_free(old);
+
+	return 0;
+}
+
+static long user_ringbuf_callback(struct bpf_dynptr *dynptr, void *ctx)
+{
+	s32 cpu = bpf_get_smp_processor_id();
+	long err;
+	enum bpf_dag_msg_type type;
+
+	bpf_printk("[*] user_ringbuf_callback: cpu=%d", cpu);
+
+	err = bpf_dynptr_read(&type, sizeof(type), dynptr, 0, 0);
+	if (err) {
+		bpf_printk("Failed to drain message type.");
+		return 1; // stop continuing
+	}
+
+	if (type == BPF_DAG_MSG_NEW_TASK) {
+		struct bpf_dag_msg_new_task_payload payload;
+
+		err = bpf_dynptr_read(&payload, sizeof(payload), dynptr, sizeof(type), 0);
+		if (err) {
+			bpf_printk("Failed to drain message new task type.");
+			return 1; // stop continuing
+		}
+		
+		err = handle_new_dag_task(&payload);
+		if (err) {
+			bpf_printk("Failed to handle a new dag task message");
+			return 1;
+		}
+
+	} else if (type == BPF_DAG_MSG_ADD_NODE) {
+		struct bpf_dag_msg_add_node_payload payload;
+
+		err = bpf_dynptr_read(&payload, sizeof(payload), dynptr, sizeof(type), 0);
+		if (err) {
+			bpf_printk("Failed to drain message add node.");
+			return 1; // stop continuing
+		}
+		
+		err = handle_add_node(&payload);
+		if (err) {
+			bpf_printk("Failed to handle add_node message");
+			return 1;
+		}
+
+	} else if (type == BPF_DAG_MSG_ADD_EDGE) {
+		struct bpf_dag_msg_add_edge_payload payload;
+
+		err = bpf_dynptr_read(&payload, sizeof(payload), dynptr, sizeof(type), 0);
+		if (err) {
+			bpf_printk("Failed to drain message add edge.");
+			return 1; // stop continuing
+		}
+		
+		err = handle_add_edge(&payload);
+		if (err) {
+			bpf_printk("Failed to handle add_edge message");
+			return 1;
+		}
+
+	} else {
+		bpf_printk("[ WARN ] Unknown message type: BPF_DAG_MSG_?=%d", type);
+	}
+
 	return 0;
 }
 
@@ -416,7 +559,11 @@ static long user_ringbuf_callback(struct bpf_dynptr *dynptr, void *ctx)
  */
 void consume_user_ringbuf()
 {
-	bpf_user_ringbuf_drain(&urb, user_ringbuf_callback, NULL, 0);
+	// User ringbuf messages are only consumed by CPU0.
+	s32 cpu = bpf_get_smp_processor_id();
+	if (cpu == 0) {
+		bpf_user_ringbuf_drain(&urb, user_ringbuf_callback, NULL, 0);
+	}
 }
 
 // MARK: init/exit
