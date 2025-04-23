63d55c0f9b36da8929b1c1a3f66afcd7bb772599
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2025-04-23 12:47:15 +0900

    Make set-IRQs-affinity.sh more flexible.
    Changes:
    - Allow specifying IRQ affinities via a command line argument
    - Skip IRQs whose affinity files are not writable
    
    Usage:
    ```
    $ sudo ./set-IRQs-affinity.sh 0-2,6-8
    Affining vector 0 to CPUs 0-2,6-8
    Affining vector 1 to CPUs 0-2,6-8
    Affining vector 10 to CPUs 0-2,6-8
    Affining vector 11 to CPUs 0-2,6-8
    Affining vector 12 to CPUs 0-2,6-8
    [...]
    Affining vector 146 to CPUs 0-2,6-8
    Skipping IRQ 147 (mode -r--r--r--: definitely not writable)
    Skipping IRQ 148 (mode -r--r--r--: definitely not writable)
    Skipping IRQ 149 (mode -r--r--r--: definitely not writable)
    Affining vector 15 to CPUs 0-2,6-8
    [...]
    ```

--- scheduler/set-IRQs-affinity.sh
+++ scheduler/set-IRQs-affinity.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-# Migrate irqs to CPU 0-4,6-10 (exclude CPU 5,11)
-for I in $(ls /proc/irq)
-do
-    if [[ -d "/proc/irq/$I" ]]
-    then
-        echo "Affining vector $I to CPUs 0-4,6-10"
-        echo "0-4,6-10" > /proc/irq/$I/smp_affinity_list
-    fi
+if [[ $EUID -ne 0 ]]; then
+	echo "Please run this script with sudo."
+	exit 1
+fi
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <cpu_list>"
+	echo "Example: $0 0-4,6-10"
+	exit 1
+fi
+
+CPU_LIST=$1
+
+# Migrate irqs to the specified CPU list
+for I in $(ls /proc/irq); do
+	AFFINITY_FILE="/proc/irq/$I/smp_affinity_list"
+
+	if [[ -d "/proc/irq/$I" && -f "$AFFINITY_FILE" ]]; then
+		# If the affinity file is not writable by root.
+		# This scripts is supposed to be executed by root user.
+		MODE=$(stat -c "%A" "$AFFINITY_FILE")
+		if [[ "$MODE" == "-r--r--r--" ]]; then
+			echo "Skipping IRQ $I (mode $MODE: definitely not writable)"
+			continue
+		fi
+
+		echo "Affining vector $I to CPUs $CPU_LIST"
+		echo "$CPU_LIST" > "$AFFINITY_FILE"
+	fi	
 done
